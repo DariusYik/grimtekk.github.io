1672f6d1a713587b5b0c3468075929344c016fba
Darius Yik <darius.yik@gmail.com>
2019-03-29 23:50:08 -0700

    cleaned up code format

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // Scroll effect
-    $(".navbar a, .page-footer a").click(function(){
+    $(".navbar a, .page-footer a").click(function () {
         $("body,html").animate({
-            scrollTop:$("#" + $(this).data('value')).offset().top
-        },1000)
+            scrollTop: $("#" + $(this).data('value')).offset().top
+        }, 1000)
 
     });
 
     // Header Image
     $('.header').height($(window).height());
-    window.onresize = function() { // re-size accordingly
+    window.onresize = function () { // re-size accordingly
         $('.header').height($(window).height());
     };
 })
