1c0c9b4ad1f403ffde79d34509d7140165de2f0d
Darius Yik <darius.yik@gmail.com>
2019-03-30 17:06:15 -0700

    Added navigation animation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 $(document).ready(function () {
 
+    $(".navbar-brand").hide();
+
     // Scroll effect
-    $(".navbar a, .page-footer a").click(function () {
+    // interferes with nav background behaviour
+    /*$(".navbar a, .page-footer a").click(function () {
         $("body,html").animate({
             scrollTop: $("#" + $(this).data('value')).offset().top
         }, 1000)
 
-    });
+    });*/
 
     // Header Image
     $('.header').height($(window).height());
     window.onresize = function () { // re-size accordingly
         $('.header').height($(window).height());
     };
+
+    //Change nav background and show Home when scrolling
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        if($(this).scrollTop() > $nav.height()) {
+            $(".navbar-brand").fadeIn();
+        } else {
+            $(".navbar-brand").fadeOut();
+        }
+    });
 })
